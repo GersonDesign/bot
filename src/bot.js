0f8d0faebfe4611734c9161a7d74c3ff11d12ad4
Gerson Perez <gperezdesign@gmail.com>
2017-08-15 18:28:21 -0500

    Update

--- src/bot.js
+++ src/bot.js
@@ -1,27 +1,78 @@
-// Call Recast.AI SDK, through /converse route
-request.converseText(text, { conversationToken: senderId })
-.then(result => {
-  /*
-  * YOUR OWN CODE
-  * Here, you can add your own process.
-  * Ex: You can call any external API
-  * Or: Update your mongo DB
-  * etc...
-  */
-  if (result.action) {
-    console.log('The conversation action is: ', result.action.slug)
-  }
+/*
+ * bot.js
+ *
+ * In this file:
+ * - received message from a connected channel will be transformed with Recast.AI SDK
+ * - received message from test command will be processed by Recast.AI
+ *   You can run this command for testing:
+ *   curl -X "POST" "http://localhost:5000" -d '{"text": "YOUR_TEXT"}' -H "Content-Type: application/json; charset=utf-8"
+ *
+ *
+ * The Recast.AI SDK will handle the message and call your reply bot function (ie. replyMessage function)
+ */
 
-  // If there is not any message return by Recast.AI for this current conversation
-  if (!result.replies.length) {
-    message.addReply({ type: 'text', content: 'I don't have the reply to this yet :)' })
+const recastai = require('recastai').default
+
+const replyMessage = require('./message')
+
+// Instantiate Recast.AI SDK
+const client = new recastai(process.env.REQUEST_TOKEN)
+
+/*
+ * Main bot function
+ * Parameters are:
+ * - body: Request body
+ * - response: Response of your server (can be a blank object if not needed: {})
+ * - callback: Callback is a function called by Recast.AI hosting system when your code will be hosted
+ */
+export const bot = (body, response, callback) => {
+  if (body.message) {
+    /*
+    * Call the Recast.AI SDK function to handle message from Bot Connector
+    * This function will:
+    * - Return a response with the status code 200
+    * - Create a Message object, easily usable in your code
+    * - Call the 'replyMessage' function, with this Message object in parameter
+    *
+    * If you want to edit the behaviour of your code bot, depending on user input,
+    * go to /src/message.js file and write your own code under "YOUR OWN CODE" comment.
+    */
+    client.connect.handleMessage({ body }, response, replyMessage)
+
+    /*
+     * This function is called by Recast.AI hosting system when your code will be hosted
+     */
+    callback(null, { result: 'Bot answered :)' })
+  } else if (body.text) {
+    /*
+    * If your request comes from the testing route
+    * ie curl -X "POST" "https://localhost:5000" -d '{"text": "YOUR_TEXT"}' -H "Content-Type: application/json; charset=utf-8"
+    * It just sends it to Recast.AI and returns replies
+    */
+    client.request.converseText(body.text, { conversationToken: process.env.CONVERSATION_TOKEN || null })
+      .then((res) => {
+        if (res.reply()) {
+          /*
+           * If response received from Recast.AI contains a reply
+           */
+          callback(null, {
+            reply: res.reply(),
+            conversationToken: res.conversationToken,
+          })
+        } else {
+          /*
+           * If response received from Recast.AI does not contain any reply
+           */
+          callback(null, {
+            reply: 'No reply :(',
+            conversationToken: res.conversationToken,
+          })
+        }
+      })
+      .catch((err) => {
+        callback(err)
+      })
   } else {
-    // Add each reply received from API to replies stack
-    result.replies.forEach(replyContent => message.addReply({ type: 'text', content: replyContent }))
+    callback('No text provided')
   }
-
-  // Send all replies
-  message.reply()
-  .then(() => {
-    // Do some code after sending messages
-  })
+}
